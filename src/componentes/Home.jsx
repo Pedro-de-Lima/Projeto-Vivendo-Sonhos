d2ca6b51cde24acc5917e20d4a9690cbb6a5f4ee
Pedro-de-Lima <luizp1163@gmail.com>
2024-06-12 14:25:41 -0300

    Parte da home do site quase finalizada, faltando a imagem e alguns detalhes

---
+++ src/componentes/Home.jsx
@@ -0,0 +1,19 @@
+export default function Home() {
+    return(
+        <>
+            <div className="Home">
+                <div className="Texto">
+                    <h1>Bem-vindo ao</h1>
+                    <h2>INSTITUTO <br></br>VIVENDO<br></br>SONHOS</h2>
+                    <p>BREVE TEXTO SOBRE A INTITUIÇÃO</p>
+                    <div>WhatsApp</div>
+                </div>
+                <div className="Quadrado">
+                    <div className="Janela">
+                        <div className="Logo"></div>
+                    </div>
+                </div>    
+            </div>   
+        </>
+    )
+}
