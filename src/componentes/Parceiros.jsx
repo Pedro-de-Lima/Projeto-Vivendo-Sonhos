93a766a0d5edaba86722f8949c0564cea9fc2471
Pedro de Lima <luizp1163@gmail.com>
2024-07-23 17:20:27 -0300

    algumas modificações no arquivo Parceiros.jsx

--- src/componentes/Parceiros.jsx
+++ src/componentes/Parceiros.jsx
@@ -1,8 +1,12 @@
 export default function Parceiros() {
+    
     return(
         <>
-            <div>
-                
+            <div style={{width:'100%', height: '100vh', display: 'grid', gridTemplateColumns: 'repeat(2, 1fr)', gridTemplateRows:'repeat'}}>
+                <img src="https://dummyimage.com/300x300/000/fff" alt="" />
+                <img src="https://dummyimage.com/300x300/000/fff" alt="" />
+                <img src="https://dummyimage.com/300x300/000/fff" alt="" />
+                <img src="https://dummyimage.com/300x300/000/fff" alt="" />
             </div>
         </>
     )
