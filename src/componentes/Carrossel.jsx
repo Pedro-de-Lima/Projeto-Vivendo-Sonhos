5049f5ea5cc8bd140a7f23540f91eb52cd6a5181
Aluno <luizp1163@gmail.com>
2024-08-27 17:43:02 -0300

    algumas alterações no visual do site

--- src/componentes/Carrossel.jsx
+++ src/componentes/Carrossel.jsx
@@ -11,7 +11,7 @@ import Tesoureira from '../assets/Tesoureira.jpeg'
 export default function Carrossel() {
     return(
         <>
-            <Carousel id='carrossel' style={{width:'100%', height:'100vh',backgroundColor:'grey', display:'flex', alignItems:'center', justifyContent: 'center'}}>
+            <Carousel id='carrossel' style={{width:'100%', height:'100vh',backgroundColor:'#103C8F', display:'flex', alignItems:'center', justifyContent: 'center'}}>
                 <Carousel.Item>
                     <ItemCarrossel cargo='Presidente' texto='Descrição do cargo'></ItemCarrossel>
                 </Carousel.Item>
--- src/componentes/ItemCarrossel.jsx
+++ src/componentes/ItemCarrossel.jsx
@@ -2,9 +2,9 @@ export default function ItemCarrossel({cargo, texto, img}) {
 
     return(
         <>
-            <div style={{width: '70%', height:'75vh', backgroundColor:'white', display: 'flex', margin: 'auto'}}>
-                <div className="imagem" style={{width:'50%', display: 'flex', justifyContent: 'center', alignItems:'center', backgroundColor: 'blue'}}>
-                    <img src={img} style={{width: '100%'}}></img>
+            <div style={{width: '70%', height:'75vh', backgroundColor:'white', display: 'flex', margin: 'auto', borderRadius: '10px'}}>
+                <div className="imagem" style={{width:'50%', display: 'flex', justifyContent: 'center', alignItems:'center', backgroundColor: 'grey', borderRadius: '10px 0 0 10px'}}>
+                    <img src={img} style={{width: '100%'q}}></img>
                 </div>
                 <div className="texto" style={{width:'50%', display: 'flex', flexDirection:'column', justifyContent: 'center', alignItems:'center'}}>
                     <h1>{cargo}</h1>
