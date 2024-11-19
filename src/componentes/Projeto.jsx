5229ad44438c7d30ff82c2f8f6137c2c533b90cd
Pedro de Lima <luizp1163@gmail.com>
2024-11-19 10:14:54 -0300

    dando continuidade na parte de projetos do site e fiz algumas modificações na parte da diretoria

--- src/componentes/Projeto.jsx
+++ src/componentes/Projeto.jsx
@@ -15,10 +15,12 @@ export default function Projeto() {
                         <p>Adolecencia e <br></br>Tecnologia</p>
                     </div>
                 </div>
-                <div className="Quadrado">
+                <div className="descricao">
+                    <div className='detalhe_lateral'>
                     <p>A oficina de futebol, promovida pelo Instituto Vivendo Sonhos, é uma iniciativa voltada para o desenvolvimento de crianças de 6 a 12 anos, realizada aos sábados pela manhã. Muito mais do que apenas uma atividade física, o projeto busca proporcionar um ambiente onde os participantes possam aprender importantes lições de vida através do esporte, preparando-os para enfrentar os desafios do futuro com ética e responsabilidade.</p>
                     <p>O projeto "Arte de Cozinhar", desenvolvido pelo Instituto Vivendo Sonhos, é uma iniciativa voltada para a qualificação de mulheres da comunidade do bairro Campina Grande, na região Noroeste de Campinas. Com o objetivo de promover uma alimentação mais saudável, prática e saborosa, o projeto pretende atender 15 mulheres, oferecendo a elas não apenas conhecimentos culinários, mas também a oportunidade de melhorar a qualidade de vida de suas famílias por meio da alimentação.</p>
                     <p>O projeto "QI dos Sonhos" do Instituto Vivendo Sonhos, localizado na região Noroeste de Campinas, tem como principal objetivo atender crianças e adolescentes de 6 a 14 anos que se encontram em situação de vulnerabilidade social. As atividades são realizadas no período oposto ao escolar, nas segundas, quartas e sextas-feiras, e incluem uma combinação de cultura, lazer e serviços de convivência.</p>
+                    </div>
                 </div>
             </div>
         </>
